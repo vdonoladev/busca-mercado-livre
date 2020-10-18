2e87a2008489f8799d02e8daf633101098b49b67
vdonoladev <victordonolaf@gmail.com>
2020-10-18 17:44:28 -0300

    Updates

--- buscamercadolivre.py
+++ buscamercadolivre.py
@@ -4,33 +4,35 @@ import codecs
 import urllib.request
 import urllib.parse
 
+
 def usage():
-	print('Uso: {0} "PRODUTO"'.format(sys.argv[0]))
-	print('Busque um produto por vez')
-	sys.exit(1)
+    print('Uso: {0} "PRODUTO"'.format(sys.argv[0]))
+    print('Busque um produto por vez')
+    sys.exit(1)
+
 
 def busca(item):
-	url = 'https://api.mercadolibre.com/sites/MLB/search?q={0}'.format(item)
-	opener = urllib.request.build_opener()
-	opener.addheaders = [
-		('User-agent', "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201")]
+    url = 'https://api.mercadolibre.com/sites/MLB/search?q={0}'.format(item)
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        ('User-agent', "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201")]
 
-	with opener.open(url) as fd:
-		content = fd.read()
-		encoding = fd.info().get_content_charset()
-		content = content.decode(encoding)
+    with opener.open(url) as fd:
+        content = fd.read()
+        encoding = fd.info().get_content_charset()
+        content = content.decode(encoding)
 
-	dic	= json.loads(content)
+    dic = json.loads(content)
 
-	sys.stdout = codecs.getwriter('UTF-8') (sys.stdout.detach())
-	for elem in dic ['results']:
-		print('{0:<70}R${1}\n{2}\n'.format(elem['title'],
-										   elem['price'],
-										   elem['permalink']))
+    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout.detach())
+    for elem in dic['results']:
+        print('{0:<70}R${1}\n{2}\n'.format(elem['title'],
+                                           elem['price'],
+                                           elem['permalink']))
+
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1 or sys.argv[1] in {'-h', '--help'}:
-		usage()
+    if len(sys.argv) == 1 or sys.argv[1] in {'-h', '--help'}:
+        usage()
 
-	busca(urllib.parse.quote_plus(' '.join(sys.argv[1:])))
-	
+    busca(urllib.parse.quote_plus(' '.join(sys.argv[1:])))
